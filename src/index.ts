cd22bbb8aa3efef5eef60a37ea76ea3cc0d99c2a
youngchae <mathrhino1@gmail.com>
2020-08-17 14:58:32 +0900

    button

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,69 @@
 import {
-  JupyterFrontEnd,
-  JupyterFrontEndPlugin
+  IDisposable, DisposableDelegate
+} from '@lumino/disposable';
+
+import {
+  JupyterFrontEnd, JupyterFrontEndPlugin
 } from '@jupyterlab/application';
 
+import {
+  ToolbarButton
+} from '@jupyterlab/apputils';
+
+import {
+  DocumentRegistry
+} from '@jupyterlab/docregistry';
+
+import {
+  NotebookActions, NotebookPanel, INotebookModel
+} from '@jupyterlab/notebook';
+
+
 /**
- * Initialization data for the jupyterlab_hello extension.
+ * The plugin registration information.
  */
-const extension: JupyterFrontEndPlugin<void> = {
-  id: 'jupyterlab-hello',
-  autoStart: true,
-  activate: (app: JupyterFrontEnd) => {
-    console.log('JupyterLab extension jupyterlab-hello is activated!');
-  }
+const plugin: JupyterFrontEndPlugin<void> = {
+  activate,
+  id: 'my-extension-name:buttonPlugin',
+  autoStart: true
 };
 
-export default extension;
+
+/**
+ * A notebook widget extension that adds a button to the toolbar.
+ */
+export
+class ButtonExtension implements DocumentRegistry.IWidgetExtension<NotebookPanel, INotebookModel> {
+  /**
+   * Create a new extension object.
+   */
+  createNew(panel: NotebookPanel, context: DocumentRegistry.IContext<INotebookModel>): IDisposable {
+    let callback = () => {
+      NotebookActions.runAll(panel.content, context.sessionContext);
+    };
+    let button = new ToolbarButton({
+      className: 'myButton',
+      iconClass: 'fa fa-fast-forward',
+      onClick: callback,
+      tooltip: 'Run All'
+    });
+
+    panel.toolbar.insertItem(0, 'runAll', button);
+    return new DisposableDelegate(() => {
+      button.dispose();
+    });
+  }
+}
+
+/**
+ * Activate the extension.
+ */
+function activate(app: JupyterFrontEnd) {
+  app.docRegistry.addWidgetExtension('Notebook', new ButtonExtension());
+};
+
+
+/**
+ * Export the plugin as default.
+ */
+export default plugin;
